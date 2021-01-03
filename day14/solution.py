b39a07274704e7149541d1c85c50fd056a83f2b8
bethlamacchia <46002319+bethlamacchia@users.noreply.github.com>
2021-01-03 10:37:42 -0500

    outlines for remaining days

---
+++ day14/solution.py
@@ -0,0 +1,20 @@
+
+"""
+Bitmasks
+part 1:
+Execute the initialization program. What is the sum of all values left in memory after it completes?
+
+Part 2
+
+"""
+
+def part_one():
+    return
+
+def part_two():
+    return
+
+
+if __name__ == "__main__":
+    with open('input.txt') as f:
+        inp = f.readlines()
---
+++ day23/solution.py
@@ -0,0 +1,18 @@
+
+"""
+Part 1
+
+Part 2
+
+"""
+
+def part_one():
+    return
+
+def part_two():
+    return
+
+
+if __name__ == "__main__":
+    with open('input.txt') as f:
+        inp = f.readlines()
